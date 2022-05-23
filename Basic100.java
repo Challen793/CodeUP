5e6e5d6e2170014dee7923ad80b67788281c9428
qkralstkd123@gmail.com <qkralstkd123@gmail.com>
2022-05-23 23:24:19 +0900

    Update Matrix Code [Basic100.java]

--- Basic100.java
+++ Basic100.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Basic100 {
 
@@ -8,6 +6,97 @@ public class Basic100 {
 
     }
 }
+
+    /* 코드업 1098번 문제
+    * 2022-05-23
+    * 2차원배열에 가로,세로 막대 놓기
+    public static void main(String args[]) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = null;
+        str = br.readLine().split(" ");
+
+        //전체 배열 크기 입력 및 0으로 초기화
+        int width = Integer.parseInt(str[0].trim());
+        int height = Integer.parseInt(str[1].trim());
+        int[][] matrix = new int[width][height];
+
+        //막대의 개수 만큼 반복
+        int num = Integer.parseInt(br.readLine());
+        for(int k=0; k<num; k++){
+
+            //길이 len, 방향 direction(가로:0, 세로:1), 좌표(x,y)
+            str = br.readLine().split(" ");
+            int len = Integer.parseInt(str[0].trim());
+            int direction = Integer.parseInt(str[1].trim());
+            int x = Integer.parseInt(str[2].trim()) - 1;
+            int y = Integer.parseInt(str[3].trim()) - 1;
+
+            if(direction == 0){ //가로방향으로 막대 놓기
+                for(int i=y; i<y+len; i++){
+                    matrix[x][i] = 1;
+                }
+            }
+            else if(direction == 1){ //세로방향으로 막대 놓기
+                for(int i=x; i<x+len; i++){
+                    matrix[i][y] = 1;
+                }
+            }
+        }
+
+        //배열 출력
+        for(int i=0; i<width; i++){
+            for(int j=0; j<height; j++){
+                System.out.print(matrix[i][j]);
+                System.out.print( j == height-1 ? "" : " ");
+            }
+            System.out.print( i == width-1 ? "" : "\n");
+        }
+    }*/
+
+
+    /* 코드업 1097번 문제
+    * 2022-05-23
+    * 2차원배열 십자가 모양으로 뒤집기
+    public static void main(String args[]) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[][] matrix = new int[19][19];
+        String[] str = null;
+
+        //배열을 입력 받음
+        for( int i = 0; i < 19; i++){
+            str = br.readLine().split(" ");
+            for( int j = 0; j < 19; j++){
+                matrix[i][j] = Integer.parseInt(str[j].trim());
+            }
+        }
+
+        //몇번 십자가 할지
+        int num = Integer.parseInt(br.readLine());
+        for( int k = 0; k < num; k++ ){
+            str = br.readLine().split(" ");
+            int x = Integer.parseInt(str[0].trim()) - 1; //십자가 중심 x축
+            int y = Integer.parseInt(str[1].trim()) - 1; //십자가 중심 y축
+
+            //십자 뒤집기 수행
+            for( int i = 0; i < 19; i++ ){
+                matrix[x][i] = (matrix[x][i] == 0 ? 1 : 0); //가로
+                matrix[i][y] = (matrix[i][y] == 0 ? 1 : 0); //세로
+            }
+        }
+
+        //배열 출력해보기
+        for( int i = 0; i < 19; i++){
+            for( int j = 0; j < 19; j++){
+                System.out.print(matrix[i][j]);
+                System.out.print( j == 19-1 ? "" : " ");
+            }
+            System.out.print( i == 19-1 ? "" : "\n");
+        }
+    }*/
+
+
 
     /* 코드업 1082번 문제
     * 2022-05-19
